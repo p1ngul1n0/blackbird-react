dbfa963d032f59591d460c4d5ef02402136d2fbe
p1ngul1n0 <57546870+p1ngul1n0@users.noreply.github.com>
2022-11-27 21:16:00 -0300

    Added support to Heroku APP

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,2 +1,8 @@
-// base path
-export const basePath = "http://127.0.0.1:9797/";
+// local path
+export let basePath;
+
+if (window.location.hostname == "blackbird-osint.herokuapp.com") {
+  basePath = "https://blackbird-osint.herokuapp.com/";
+} else {
+  basePath = "http://127.0.0.1:9797/";
+}
