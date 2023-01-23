1f708602d5ce226d41023e38c25c769217743275
Pitastic <mickep1985@gmail.com>
2023-01-23 14:08:30 +0100

    API basePath is now variable

--- src/Config/api.js
+++ src/Config/api.js
@@ -4,6 +4,7 @@ export let cloudDomain;
 cloudDomain = "blackbird-osint.herokuapp.com";
 if (window.location.hostname == cloudDomain) {
   basePath = "https://" + cloudDomain + "/";
-} else {
-  basePath = "http://127.0.0.1:9797/";
+}else{
+  basePath = window.location.protocol + "://" +
+             window.location.host + ":" + "/";
 }
